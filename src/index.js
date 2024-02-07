5a7b8a3c0f3a512da4eed562adb13c8453026ef8
Mike-Benn <mbenn49@wgu.edu>
2024-02-07 04:17:28 -0500

    Import modules and assign to eventlisteners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('Test');
+import { homeActive } from "./home.js";
+import { contactActive } from "./contact.js";
+
+
+function screenController() {
+    const header = document.querySelector('.header');
+
+    const nav = document.createElement('nav');
+    nav.classList.add('top-nav');
+  
+    const homeBtn = document.createElement('button');
+    const contactBtn = document.createElement('button');
+    homeBtn.type = 'button';
+    contactBtn.type = 'button';
+    homeBtn.textContent = 'Home';
+    contactBtn.textContent = 'Contact';
+    homeBtn.addEventListener('click' , homeActive);
+    contactBtn.addEventListener('click' , contactActive);
+  
+    nav.appendChild(homeBtn);
+    nav.appendChild(contactBtn);
+    console.log(header);
+    header.appendChild(nav);
+
+    homeActive();
+
+
+
+}
+
+screenController();
